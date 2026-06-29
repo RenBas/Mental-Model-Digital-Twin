53c79c6d9b6bc4072e32c522b5cfb85b7773d3c0
RenBas <renbas9992000@gmail.com>
2026-06-29 14:31:59 +0800

    Update edge.py

--- models/edge.py
+++ models/edge.py
@@ -1,1 +1,6 @@
-
+class MentalModelEdge:
+    def __init__(self, source_name, target_name, coefficient, r_square):
+        self.source_name = source_name
+        self.target_name = target_name
+        self.coefficient = coefficient
+        self.r_square = r_square
